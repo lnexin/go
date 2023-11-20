1cffb25a7acab9997fd2a10d0c4198521c50b075
duxin <leo.du@backgardon.com>
2023-11-20 10:32:35 +0800

    optimize

---
+++ main/ex_03_variable.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "ddddd-hello world"
+	fmt.Println(a)
+
+	var a1, b, c int = 1, 2, 3
+	fmt.Println(a1, b, c)
+
+	var f = true
+	fmt.Println(f)
+	fmt.Println(!f)
+	fmt.Println(f)
+
+	var e string
+	fmt.Println(e)
+
+	cd := "sds"
+	fmt.Println(cd)
+}
